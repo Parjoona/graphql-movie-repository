51bba12b3f63e9062d13f284b718d9e4304da1ab
parjoona <parjoona@hotmail.com>
2019-03-19 20:40:56 +0100

    added sqlite to json

--- server/database.mjs
+++ server/database.mjs
@@ -1,8 +1,13 @@
 import sqlite3 from 'sqlite3'
+const exporter = new SqliteToJson({
+  client: new sqlite3.Database('./database.db')
+});
 
-sqlite3.verbose()
-let database = new sqlite3.Database('./database.db')
+exporter.tables((err, tables) => {
+  if (err) return console.log(err.message)
+})
 
+// Closing the database whenever its done querying
 database.close(err => err ? console.error(err.message) : console.log('Database connection closed'))
 
 export default database
